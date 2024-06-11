a4a99525c02e952936b97af49ae20d66c590ebb0
Maria Theresa de Sousa Ferreira <maria.s.ferreira@equatorialenergia.com.br>
2024-06-11 14:17:07 -0300

    Criação do app DriverAt

---
+++ comissionamento/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ComissionamentoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'comissionamento'
---
+++ comissionamento/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class DriverAt(models.Model):
+    ip = models.CharField(max_length=20)
+    porta = models.IntegerField()
+    dnp = models.IntegerField()
+    range_bi = models.CharField(max_length=10)
+    range_ai = models.CharField(max_length=10)
+    range_bo = models.CharField(max_length=10)
+    faixa_ip_se = models.CharField(max_length=20)
+    data_de_criacao = models.DateTimeField(blank=True, null=True)
+
+    def criarDriverAt(self):
+        self.data_de_criacao = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.ip
